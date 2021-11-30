e4aa98131a34f1ca25afef1938d50e418f4ae0c4
Kevin Smith <ksmithbaylor@gmail.com>
2021-11-30 10:29:53 -0500

    Fix typescript date issues

--- frontend/src/components/SyncInfo.tsx
+++ frontend/src/components/SyncInfo.tsx
@@ -1,14 +1,13 @@
 import { useState, useEffect } from 'react';
 import { useBlockNumber, useBlockMeta } from '@usedapp/core';
-import { formatDistance } from 'date-fns';
 import useInterval from 'react-useinterval';
 
 export function SyncInfo() {
   const [now, setNow] = useState<Date>(new Date());
   const block = useBlockNumber();
   const { timestamp } = useBlockMeta();
-  const blockDate = new Date(timestamp);
-  const ageMS = now - blockDate;
+  const blockDate = timestamp ? new Date(timestamp) : new Date();
+  const ageMS = Number(now) - Number(blockDate);
   const ageSeconds = Math.round(ageMS / 1000);
 
   useInterval(() => setNow(new Date()), 100);
